b4cf987c8cc961807f778b9802b48bdaa53e36db
Igor Lunin <lunigorn@gmail.com>
2024-11-03 12:26:31 +0100

    Add disabled test

---
+++ src/test/java/com/craftens/totalreport/junit5example/DisabledTestTest.java
@@ -0,0 +1,49 @@
+package com.craftens.totalreport.junit5example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DisabledTestTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        log.info("beforeAll");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        log.info("beforeEach");
+    }
+
+    @Test
+    public void test1() {
+        log.info("Test method1");
+    }
+
+    @Disabled
+    @Test
+    public void test2() {
+        log.info("Test method2");
+    }
+
+    @Test
+    public void test3() {
+        log.info("Test method3");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        log.info("afterEach");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        log.info("afterAll");
+    }
+}
