af0e848507f26c1fe1ee845a7df708c18ab02d8f
Igor Lunin <lunigorn@gmail.com>
2024-11-23 13:46:10 +0100

    Add timeout in before all

---
+++ src/test/java/com/craftens/totalreport/junit5example/TimeoutInBeforeAllTest.java
@@ -0,0 +1,45 @@
+package com.craftens.totalreport.junit5example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class TimeoutInBeforeAllTest {
+
+    @BeforeAll
+    @Timeout(value = 1, unit = TimeUnit.NANOSECONDS)
+    public static void beforeAll() throws InterruptedException {
+        log.info("beforeAll");
+        Thread.sleep(10);
+    }
+
+    @BeforeEach
+    public void beforeEach() throws InterruptedException {
+        log.info("beforeEach");
+        Thread.sleep(1);
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        log.info("Test method");
+        Thread.sleep(1);
+    }
+
+    @AfterEach
+    public void afterEach() throws InterruptedException {
+        log.info("afterEach");
+        Thread.sleep(1);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        log.info("afterAll");
+    }
+}
